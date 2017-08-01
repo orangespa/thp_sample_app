441d41d62981aa1845f9eff58386d785df79dd60
hong <luu.bichhong@gmail.com>
2017-08-01 15:34:55 +0200

    finnish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,9 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user ==current_user
+  end
 
   def logged_in?
     !current_user.nil?
@@ -42,6 +45,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+   # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
